1313ded050f05cbce2d82681fe208ec55c3be4d7
Gaurav kedia <Gaurav.kedia18@gmail.com>
2024-02-11 10:40:46 +0530

    a7

--- Scripts/Deploy-Script.sh
+++ Scripts/Deploy-Script.sh
@@ -3,5 +3,10 @@ cd
 cd /home/ubuntu/java-SB-1/test-env-foreverjava-javaSB/Scripts
 ls -l
 pwd
-echo "********** End of Deploy Script .sh file **********"
-echo "********** End of Deploy Script .sh file **********"
+echo "********** End of Deploy Script .sh file 1 **********"
+echo "********** End of Deploy Script .sh file 2 **********"
+echo "********** End of Deploy Script .sh file 3 **********"
+echo "********** End of Deploy Script .sh file 4 **********"
+echo "********** End of Deploy Script .sh file 5 **********"
+echo "********** End of Deploy Script .sh file 6 **********"
+echo "********** End of Deploy Script .sh file 7 **********"
