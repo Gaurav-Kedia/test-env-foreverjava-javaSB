ae496a18047efa8b884228de7423467a6c560c12
Gaurav kedia <Gaurav.kedia18@gmail.com>
2024-02-11 10:34:13 +0530

    modified deploy script sh file

--- Scripts/Deploy-Script.sh
+++ Scripts/Deploy-Script.sh
@@ -1,5 +1,6 @@
-echo "**** Running Deploy Script .sh file"
+echo "********** Running Deploy Script .sh file **********"
 cd
 cd /home/ubuntu/java-SB-1/test-env-foreverjava-javaSB/Scripts
 ls -l
 pwd
+echo "********** End of Deploy Script .sh file **********"
